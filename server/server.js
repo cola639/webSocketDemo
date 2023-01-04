f4a66d747f6a7dbdbe10a3237307b90af40827a2
cola639 <337507950@qq.com>
2023-01-04 17:03:09 +0800

    add：

--- server/server.js
+++ server/server.js
@@ -3,12 +3,11 @@ const app = express()
 const expressWs = require('express-ws')
 const websocket = require('./websocket')
 
-expressWs(app);
+expressWs(app)
 
 app.use(express.static('public'))
 app.use('/websocket', websocket)
 app.get('*', (req, res) => {})
-app.listen(3000, () => {
-  console.log('server is listening on port 3000')
+app.listen(3100, () => {
+  console.log('server is listening on port 3100')
 })
-
